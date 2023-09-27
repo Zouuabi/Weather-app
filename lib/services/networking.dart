5f2c61b99a3ba91591c2dc1425a2466fd1294495
Zouuabi <95693347+Zouuabi@users.noreply.github.com>
2023-09-27 01:14:10 +0100

    remarque mobile

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart' ;
 
 class NetworkHelper {
   NetworkHelper({required this.url});
@@ -7,7 +7,7 @@ class NetworkHelper {
   final String url;
 
   Future getData() async {
-    http.Response response = await http.get(Uri.parse(url));
+   Response response = await get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       String data = response.body;
